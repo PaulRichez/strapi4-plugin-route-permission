621c8437c1b8a0615b06863b5a13f6aeebaddb8f
PaulRichez <paul.richez59@gmail.com>
2022-09-26 05:38:12 +0200

    fix

--- server/services/routes.js
+++ server/services/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 const _ = require('lodash');
 
-const transformRoute = (route, type, name  ) => {
+const transformRoute = (route, type, name) => {
   var [controller, action] = _.get(route, "handler").split(".")
   if (route.handler.includes('::')) {
     // for config on core route
@@ -24,24 +24,28 @@ module.exports = ({ strapi }) => ({
     for (const apiName in strapi.api) {
       const api = strapi.api[apiName];
       _.forEach(api.routes, router => {
-        router.routes.forEach((route) => {
-          if (route?.config?.roles) {
-            const result = transformRoute(route, 'api', apiName)
-            routes.push(result);
-          }
-        })
+        if (router.routes && router.routes.length > 0) {
+          router.routes.forEach((route) => {
+            if (route?.config?.roles) {
+              const result = transformRoute(route, 'api', apiName)
+              routes.push(result);
+            }
+          })
+        }
       })
     }
     // get routes from each Plugin
     for (const pluginName in strapi.plugins) {
       const plugin = strapi.plugins[pluginName];
       _.forEach(plugin.routes, router => {
-        router.routes.forEach((route) => {
-          if (route?.config?.roles) {
-            const result = transformRoute(route, 'plugin', pluginName)
-            routes.push(result);
-          }
-        })
+        if (router.routes && router.routes.length > 0) {
+          router.routes.forEach((route) => {
+            if (route?.config?.roles) {
+              const result = transformRoute(route, 'plugin', pluginName)
+              routes.push(result);
+            }
+          })
+        }
       })
     }
 
